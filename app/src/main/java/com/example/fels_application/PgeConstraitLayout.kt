3941008dfea01152567d97430c400545ea2b19cf
gapsoliver <olivergaps51.com>
2023-05-11 14:34:05 +0300

    first commit

---
+++ app/src/main/java/com/example/fels_application/PgeConstraitLayout.kt
@@ -0,0 +1,11 @@
+package com.example.fels_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PgeConstraitLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pge_constrait_layout)
+    }
+}
